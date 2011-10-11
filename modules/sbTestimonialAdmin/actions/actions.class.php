3f24c76688125f32a0a946cd950648a67629e101
Giles Smith <giles@superrb.com>
2011-10-11 10:56:02 +0100

    Overidable actions

---
+++ modules/sbTestimonialAdmin/actions/actions.class.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/sbTestimonialAdminGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/sbTestimonialAdminGeneratorHelper.class.php';/**
+ *
+ * sbJobBoardJobAdmin actions.
+ * @package    asandbox
+ * @subpackage sbJobBoardJobAdmin
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class sbTestimonialAdminActions extends autoSbTestimonialAdminActions
+{
+}
