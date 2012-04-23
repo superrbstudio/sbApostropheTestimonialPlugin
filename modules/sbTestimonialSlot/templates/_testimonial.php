7a5cc5e726a6f41b9fa9a5b6ccddf55d8504b8ce
Alex Borton <alex@superrb.com>
2012-04-23 17:18:57 +0100

    NL2BR DDED TO testimonial output

--- modules/sbTestimonialSlot/templates/_testimonial.php
+++ modules/sbTestimonialSlot/templates/_testimonial.php
@@ -1,7 +1,7 @@
 <?php $testimonial = isset($testimonial) ? $sf_data->getRaw('testimonial') : null; ?>
 <?php if($testimonial): ?>
 <blockquote class="sb-testimonial">
-	<p class="sb-testimonial-quote">"<?php echo $testimonial['testimonial']; ?>"</p>
+	<p class="sb-testimonial-quote">"<?php echo nl2br($testimonial['testimonial']); ?>"</p>
 	<cite>
 		<span class="sb-testimonial-name"><?php echo $testimonial['name']; ?></span>
 		<span class="sb-testimonial-title"><?php echo $testimonial['job_title']; ?></span>
